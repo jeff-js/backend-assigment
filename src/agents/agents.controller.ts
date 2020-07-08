45c4e2f27830d68bf5590dd55f43ad75d3408211
Jefferson Aguilar <jefferson@alunideas.com>
2020-07-08 12:02:51 -0500

    catch errors

--- src/agents/agents.controller.ts
+++ src/agents/agents.controller.ts
@@ -1,6 +1,13 @@
-import { Controller, Body, Post, Get, Param, HttpStatus, Res } from '@nestjs/common';
+import {
+  Controller,
+  Body,
+  Post,
+  Get,
+  Param,
+  HttpStatus,
+  Res,
+} from '@nestjs/common';
 import { AgentsService } from './agents.service';
-import { AgentModel } from './models/agent.model';
 import { AgentDto } from './dto/agent.dto';
 import { AsignIssueDto } from './dto/asignamentIssue.dto';
 import { IssuesService } from 'src/issues/issues.service';
@@ -8,12 +15,27 @@ import { Response } from 'express';
 
 @Controller('agents')
 export class AgentsController {
-
-  constructor(private readonly agentService: AgentsService, private readonly issuesService: IssuesService) {}
+  constructor(
+    private readonly agentService: AgentsService,
+    private readonly issuesService: IssuesService,
+  ) {}
 
   @Post()
-  async createAgent(@Body() body: AgentDto): Promise<AgentModel> {
-    return await this.agentService.create(body);
+  async createAgent(
+    @Body() body: AgentDto,
+    @Res() res: Response,
+  ): Promise<Response> {
+    try {
+      const agent = await this.agentService.create(body);
+      return res
+        .status(HttpStatus.OK)
+        .json({ success: true, message: 'Creación exitosa', data: agent });
+    } catch (error) {
+      return res.status(HttpStatus.OK).json({
+        success: false,
+        message: 'Ha ocurrido un error al ejecutar la operacion',
+      });
+    }
   }
 
   @Get()
@@ -22,20 +44,42 @@ export class AgentsController {
   }
 
   @Post('/:id/asign-issue')
-  async asignIssue(@Param('id') agentId: string, @Body() body: AsignIssueDto, @Res() res: Response): Promise<Response>{
-    await this.agentService.asignIssue(agentId, body.issueAsigned);
-    return res.status(HttpStatus.OK).json({});
-  }
-
-  @Post('/:id/solve-issue')
-  async solveIssue(@Param('id') agentId: string, @Body() body: AsignIssueDto, @Res() res: Response): Promise<Response>{
+  async asignIssue(
+    @Param('id') agentId: string,
+    @Body() body: AsignIssueDto,
+    @Res() res: Response,
+  ): Promise<Response> {
     try {
-      console.log('object');
-      await this.issuesService.solveIssue(body.issueAsigned, agentId);
-    return res.status(HttpStatus.OK).json({});
+      await this.agentService.asignIssue(agentId, body.issueAsigned);
+      return res
+        .status(HttpStatus.OK)
+        .json({ success: true, message: 'Asignación OK' });
     } catch (error) {
-      return res.status(HttpStatus.OK).json({error: 'error'});
+      return res.status(HttpStatus.OK).json({
+        success: false,
+        message: 'Ha ocurrido un error al ejecutar la operacion',
+      });
     }
   }
 
+  @Post('/:id/solve-issue')
+  async solveIssue(
+    @Param('id') agentId: string,
+    @Body() body: AsignIssueDto,
+    @Res() res: Response,
+  ): Promise<Response> {
+    try {
+      console.log('object');
+      await this.issuesService.solveIssue(body.issueAsigned, agentId);
+
+      return res
+        .status(HttpStatus.OK)
+        .json({ success: true, message: 'Asignación OK' });
+    } catch (error) {
+      return res.status(HttpStatus.OK).json({
+        success: false,
+        message: 'Ha ocurrido un error al ejecutar la operacion',
+      });
+    }
+  }
 }
--- src/issues/issues.controller.ts
+++ src/issues/issues.controller.ts
@@ -1,14 +1,28 @@
-import { Controller, Body, Post, Get } from '@nestjs/common';
+import { Controller, Body, Post, Get, Res, HttpStatus } from '@nestjs/common';
 import { IssueDto } from './dto/issue.dto';
 import { IssuesService } from './issues.service';
-import { IssueModel } from './models/issue.model';
+import { Response } from 'express';
+
 @Controller('issues')
 export class IssuesController {
   constructor(private readonly issueService: IssuesService) {}
 
   @Post()
-  async createIssue(@Body() body: IssueDto): Promise<IssueModel> {
-    return await this.issueService.create(body);
+  async createIssue(
+    @Body() body: IssueDto,
+    @Res() res: Response,
+  ): Promise<Response> {
+    try {
+      const issueCreated = await await this.issueService.create(body);
+      return res
+        .status(HttpStatus.OK)
+        .json({ success: true, message: 'Creación OK', data: issueCreated });
+    } catch (error) {
+      return res.status(HttpStatus.OK).json({
+        success: false,
+        message: 'Ha ocurrido un error al ejecutar la operacion',
+      });
+    }
   }
 
   @Get()
